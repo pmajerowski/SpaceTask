1bd52edd9a7acb02e5578d1ef32b7f90b166b0cb
pmajerowski <105345146+pmajerowski@users.noreply.github.com>
2023-05-10 17:14:06 +0200

    initial commit

---
+++ src/main/java/pl/majerowski/spacetask/SpaceTaskApplication.java
@@ -0,0 +1,13 @@
+package pl.majerowski.spacetask;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpaceTaskApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpaceTaskApplication.class, args);
+	}
+
+}
