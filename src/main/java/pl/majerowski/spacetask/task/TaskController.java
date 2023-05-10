f12da3240cafbfeb04ca334a088c91d6d9b0e7cc
pmajerowski <105345146+pmajerowski@users.noreply.github.com>
2023-05-10 18:55:07 +0200

    TaskController edited

--- src/main/java/pl/majerowski/spacetask/task/TaskController.java
+++ src/main/java/pl/majerowski/spacetask/task/TaskController.java
@@ -1,7 +1,5 @@
 package pl.majerowski.spacetask.task;
 
-import lombok.RequiredArgsConstructor;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
