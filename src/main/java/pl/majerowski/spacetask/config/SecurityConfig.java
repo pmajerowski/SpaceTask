8da47ec9eca96fd632374857c6319cf95923f9b1
pmajerowski <105345146+pmajerowski@users.noreply.github.com>
2023-06-19 20:27:17 +0200

    JwtAuthFilter bean added to security filter chain in SecurityConfig

--- src/main/java/pl/majerowski/spacetask/config/SecurityConfig.java
+++ src/main/java/pl/majerowski/spacetask/config/SecurityConfig.java
@@ -1,12 +1,17 @@
 package pl.majerowski.spacetask.config;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @EnableWebSecurity
+@RequiredArgsConstructor
 public class SecurityConfig {
+
+    private final JwtAuthFilter jwtAuthFilter;
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
@@ -15,7 +20,7 @@ public class SecurityConfig {
                 .anyRequest()
                 .authenticated()
                 .and()
-                .httpBasic();
+                .addFilterBefore(jwtAuthFilter, UsernamePasswordAuthenticationFilter.class);
 
         return http.build();
     }
