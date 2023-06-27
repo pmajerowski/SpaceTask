cc7a6b07c7df9a541d20f53bcbb8052b0efdf5fe
pmajerowski <105345146+pmajerowski@users.noreply.github.com>
2023-06-27 13:28:01 +0200

    AppUserDocument added, mapping AppUser

--- src/main/java/pl/majerowski/spacetask/user/domain/AppUser.java
+++ src/main/java/pl/majerowski/spacetask/user/domain/AppUser.java
@@ -1,11 +1,15 @@
 package pl.majerowski.spacetask.user.domain;
 
+import lombok.AllArgsConstructor;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 
 import java.util.Collection;
 
+@AllArgsConstructor
 public class AppUser implements UserDetails {
+
+    private String id;
     private String email;
     private String name;
     private String password;
@@ -16,9 +20,12 @@ public class AppUser implements UserDetails {
     private boolean enabled;
 
 
+    public String getId() {
+        return id;
+    }
     @Override
-    public Collection<? extends GrantedAuthority> getAuthorities() {
-        return authorities;
+    public String getUsername() {
+        return email;
     }
 
     public String getName() {
@@ -31,8 +38,8 @@ public class AppUser implements UserDetails {
     }
 
     @Override
-    public String getUsername() {
-        return email;
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return authorities;
     }
 
     @Override
